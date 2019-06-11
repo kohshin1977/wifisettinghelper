9fd34f6e79c4d8d28963c07e2b4f5c78484e3c33
kohshin1977 <onsight.system@gmail.com>
2019-06-11 22:10:38 +0900

    最初のコミット

---
+++ app/src/main/java/com/kohshin/wifisettinghelper/SettingActivity.kt
@@ -0,0 +1,14 @@
+package com.kohshin.wifisettinghelper
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SettingActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+
+    }
+
+}
