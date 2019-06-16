220882f1ff09862bcf3183da7109b4a9bc5c679b
kohshin1977 <onsight.system@gmail.com>
2019-06-16 22:51:49 +0900

    画面遷移を変更

--- app/src/main/java/com/kohshin/wifisettinghelper/MainActivity.kt
+++ app/src/main/java/com/kohshin/wifisettinghelper/MainActivity.kt
@@ -5,9 +5,7 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.graphics.ImageFormat
-import android.graphics.SurfaceTexture
+import android.graphics.*
 import android.hardware.camera2.*
 import android.media.ImageReader
 import android.os.*
@@ -18,13 +16,13 @@ import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.view.Surface
 import android.view.TextureView
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.NumberPicker
 import android.widget.Toast
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileOutputStream
-import java.io.IOException
+import com.googlecode.tesseract.android.TessBaseAPI
+import java.io.*
+import java.nio.channels.FileChannel
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +36,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var shutterButton : ImageButton
     private lateinit var previewView   : TextureView
     private lateinit var imageReader   : ImageReader
+    private lateinit var sizeBitmap : EditText
 
     /**
      * 各種変数初期化
@@ -49,11 +48,35 @@ class MainActivity : AppCompatActivity() {
     private var cameraDevice                   : CameraDevice?           = null
     private lateinit var captureSession        : CameraCaptureSession
 
+        val DEFAULT_LANGUAGE = "eng"
+
+    lateinit var filepath: String
+    lateinit var tessBaseAPI: TessBaseAPI
+
+    val appDir = File(Environment.getExternalStorageDirectory(), "WiFiSettingHelper")
+    val filename = "wifi.png"
+
+    /**
+     * Boolean that tells me how to treat a transparent pixel (Should it be black?)
+     */
+    private val TRASNPARENT_IS_BLACK = false
+    /**
+     * This is a point that will break the space into Black or white
+     * In real words, if the distance between WHITE and BLACK is D;
+     * then we should be this percent far from WHITE to be in the black region.
+     * Example: If this value is 0.5, the space is equally split.
+     */
+    private val SPACE_BREAKING_POINT = 20.0 / 30.0
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
 
         /**
          * ストレージ読み書きパーミッションの確認
@@ -72,20 +95,19 @@ class MainActivity : AppCompatActivity() {
 
         shutterButton = findViewById(R.id.Shutter);
 
+        sizeBitmap = findViewById(R.id.editText)
+
         /**
          * シャッターボタンにイベント生成
          */
         shutterButton.setOnClickListener {
             // フォルダーを使用する場合、あるかを確認
-            val appDir = File(Environment.getExternalStorageDirectory(), "WiFiSettingHelper")
-
             if (!appDir.exists()) {
                 // なければ、フォルダーを作る
                 appDir.mkdirs()
             }
 
             try {
-                val filename = "wifi.jpg"
                 var savefile : File? = null
 
                 /**
@@ -98,7 +120,7 @@ class MainActivity : AppCompatActivity() {
                     val fos = FileOutputStream(savefile)
                     val bitmap: Bitmap = previewView.bitmap
 //                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-                    getResizedBitmap(bitmap, 300).compress(Bitmap.CompressFormat.PNG, 100, fos)
+                    getResizedBitmap(bitmap, Integer.parseInt(sizeBitmap.text.toString())).compress(Bitmap.CompressFormat.PNG, 100, fos)
                     fos.close()
                 }
 
@@ -107,9 +129,15 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this, "Saved: $savefile", Toast.LENGTH_SHORT).show()
                 }
 
+                //文字読み込み処理
+                val result = readCharacter()
+
                 //ここで次のインテントに移動
-                val intent = Intent(this, SettingActivity::class.java)
+                val intent = Intent(this@MainActivity, NextActivity::class.java)
+                intent.putExtra("result", result)
                 startActivity(intent)
+//                val intent = Intent(this, SettingActivity::class.java)
+//                startActivity(intent)
 
             } catch (e: CameraAccessException) {
                 Log.d("edulog", "CameraAccessException_Error: $e")
@@ -126,6 +154,79 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun readCharacter(): String{
+        val loadfile = File(appDir, filename)
+        var bitmap = BitmapFactory.decodeFile(loadfile.toString())
+
+        //２値化処理
+        bitmap = processBinary(bitmap)
+
+        val savefile = File(appDir, "wifi_binary.png")
+        val fos = FileOutputStream(savefile)
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos)
+        fos.close()
+
+        //読み込み処理
+        filepath = filesDir.toString() + "/tesseract/"
+
+        tessBaseAPI = TessBaseAPI()
+
+        checkFile(File(filepath + "tessdata/"))
+
+        tessBaseAPI.init(filepath, DEFAULT_LANGUAGE)
+
+        tessBaseAPI.setImage(bitmap)
+        val result = tessBaseAPI.utF8Text
+        return result
+
+
+    }
+
+    private fun checkFile(file: File) {
+        if (!file.exists() && file.mkdirs()) {
+            copyFiles()
+        }
+        if (file.exists()) {
+            val datafilepath = "$filepath" + "tessdata/eng.traineddata"
+            val datafile = File(datafilepath)
+
+            if (!datafile.exists()) {
+                copyFiles()
+            }
+        }
+    }
+
+    private fun copyFiles() {
+        try {
+            val datapath = "$filepath" + "tessdata/eng.traineddata"
+            val instream = assets.open("tessdata/eng.traineddata")
+            val outstream = FileOutputStream(datapath)
+
+            val buffer = ByteArray(1024)
+            var read: Int
+            do{
+                read = instream.read(buffer)
+                if(read == -1)
+                    break
+                outstream.write(buffer, 0, read)
+            }while(true)
+//            while ((read = instream.read(buffer)) != -1) {
+//                outstream.write(buffer, 0, read)
+//            }
+
+            outstream.flush()
+            outstream.close()
+            instream.close()
+
+            val file = File(datapath)
+            if (!file.exists()) {
+                throw FileNotFoundException()
+            }
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+
+    }
 
 
     /**
@@ -341,6 +442,116 @@ class MainActivity : AppCompatActivity() {
             width = (height * bitmapRatio).toInt()
         }
         return Bitmap.createScaledBitmap(image, width, height, true)
+    }
+
+        private fun processBinary(bitmap: Bitmap) :Bitmap{
+        //this is the image that is binarized
+        val binarizedImage = convertToMutable(bitmap)
+        // I will look at each pixel and use the function shouldBeBlack to decide
+        // whether to make it black or otherwise white
+        for (i in 0 until binarizedImage.width) {
+            for (c in 0 until binarizedImage.height) {
+                val pixel = binarizedImage.getPixel(i, c)
+                if (shouldBeBlack(pixel))
+                    binarizedImage.setPixel(i, c, Color.BLACK)
+                else
+                    binarizedImage.setPixel(i, c, Color.WHITE)
+            }
+        }
+
+        return binarizedImage
+    }
+
+
+    /**
+     * @param pixel the pixel that we need to decide on
+     * @return boolean indicating whether this pixel should be black
+     */
+    private fun shouldBeBlack(pixel: Int): Boolean {
+        val alpha = Color.alpha(pixel)
+        val redValue = Color.red(pixel)
+        val blueValue = Color.blue(pixel)
+        val greenValue = Color.green(pixel)
+        if (alpha == 0x00)
+        //if this pixel is transparent let me use TRASNPARENT_IS_BLACK
+            return TRASNPARENT_IS_BLACK
+        // distance from the white extreme
+        val distanceFromWhite = Math.sqrt(
+            Math.pow((0xff - redValue).toDouble(), 2.0) + Math.pow(
+                (0xff - blueValue).toDouble(),
+                2.0
+            ) + Math.pow((0xff - greenValue).toDouble(), 2.0)
+        )
+        // distance from the black extreme //this should not be computed and might be as well a function of distanceFromWhite and the whole distance
+        val distanceFromBlack = Math.sqrt(
+            Math.pow((0x00 - redValue).toDouble(), 2.0) + Math.pow(
+                (0x00 - blueValue).toDouble(),
+                2.0
+            ) + Math.pow((0x00 - greenValue).toDouble(), 2.0)
+        )
+        // distance between the extremes //this is a constant that should not be computed :p
+        val distance = distanceFromBlack + distanceFromWhite
+        // distance between the extremes
+        return distanceFromWhite / distance > SPACE_BREAKING_POINT
+    }
+
+    /**
+     * @author Derzu
+     *
+     * @see https://stackoverflow.com/a/9194259/833622
+     *
+     * Converts a immutable bitmap to a mutable bitmap. This operation doesn't allocates
+     * more memory that there is already allocated.
+     *
+     *
+     * @param imgIn - Source image. It will be released, and should not be used more
+     * @return a copy of imgIn, but muttable.
+     */
+    fun convertToMutable(imgIn: Bitmap): Bitmap {
+        var imgIn = imgIn
+        try {
+            //this is the file going to use temporally to save the bytes.
+            // This file will not be a image, it will store the raw image data.
+            val file = File(Environment.getExternalStorageDirectory().toString() + File.separator + "temp.tmp")
+
+            //Open an RandomAccessFile
+            //Make sure you have added uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE"
+            //into AndroidManifest.xml file
+            val randomAccessFile = RandomAccessFile(file, "rw")
+
+            // get the width and height of the source bitmap.
+            val width = imgIn.width
+            val height = imgIn.height
+            val type = imgIn.config
+
+            //Copy the byte to the file
+            //Assume source bitmap loaded using options.inPreferredConfig = Config.ARGB_8888;
+            val channel = randomAccessFile.getChannel()
+            val map = channel.map(FileChannel.MapMode.READ_WRITE, 0, (imgIn.rowBytes * height).toLong())
+            imgIn.copyPixelsToBuffer(map)
+            //recycle the source bitmap, this will be no longer used.
+            imgIn.recycle()
+            System.gc()// try to force the bytes from the imgIn to be released
+
+            //Create a new bitmap to load the bitmap again. Probably the memory will be available.
+            imgIn = Bitmap.createBitmap(width, height, type)
+            map.position(0)
+            //load it back from temporary
+            imgIn.copyPixelsFromBuffer(map)
+            //close the temporary file and channel , then delete that also
+            channel.close()
+            randomAccessFile.close()
+
+            // delete the temp file
+            file.delete()
+
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        return imgIn
     }
 
 }
--- app/src/main/java/com/kohshin/wifisettinghelper/NextActivity.kt
+++ app/src/main/java/com/kohshin/wifisettinghelper/NextActivity.kt
@@ -1,22 +1,32 @@
 package com.kohshin.wifisettinghelper
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
 import android.widget.TextView
+import java.io.File
 
 class NextActivity : AppCompatActivity(){
-    lateinit var textView : TextView
+    val appDir = File(Environment.getExternalStorageDirectory(), "WiFiSettingHelper")
+    val filename = "wifi_binary.png"
+
+    lateinit var imageViewWifi: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_next)
 
-        textView = findViewById(R.id.textView)
+        imageViewWifi = findViewById(R.id.image_view)
+
+        val loadfile = File(appDir, filename)
+        val bitmap = BitmapFactory.decodeFile(loadfile.toString())
+        imageViewWifi.setImageBitmap(bitmap)
 
         //受け取った変数を入れる
         val text = intent.getStringExtra("result")
 
-        textView.setText(text)
     }
 
 
